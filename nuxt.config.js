8cc7d42a079d5038749db49d1e3e5e60f96284de
Kenneth Lokoy <kennethlokoy@gmail.com>
2020-05-29 08:42:29 +0200

    fixed path

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
-      { rel: 'manifest', href: 'site.webmanifest'},
+      { rel: 'manifest', href: '/favicons/site.webmanifest'},
       { rel: 'stylesheet', type: 'text/css', href:"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,300&display=swap' }
     ]
