a3e4c1134909ab0871bfedd99be2bfbb5edbd75c
lokoydesign <kennethlokoy@gmail.com>
2022-03-19 16:51:37 +0100

    update 2022-03-19

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 (function() {
-  const body          = document.querySelector('body')
-  const portfolioForm = document.getElementById('portfolio-form')
-  const galleryItems  = document.querySelectorAll('.gallery-item')
+  const body = document.querySelector('body')
 
   body.classList.remove('no-js')
   body.classList.add('js')
-
-  function filterGallery(category = 'featured') {
-    for (const galleryItem of galleryItems) {
-      galleryItem.style.display = galleryItem.classList.contains(`gallery-item-${category}`) ? 'block' : 'none'
-    }
-  }
-
-  portfolioForm.addEventListener('change', function(e) {
-    filterGallery(e.target.value)
-  })
-
-  filterGallery()
 })()
